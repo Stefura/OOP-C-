bdde940d39bc6ee18652b89b1e1858cc005ff260
aruFesT <elenacom929@gmail.com>
2023-05-09 21:13:37 +0300

    1

---
+++ lab0104/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+class Element
+{
+    private int value;
+
+    public Element(int value)
+    {
+        this.value = value;
+    }
+
+    public int GetValue()
+    {
+        return value;
+    }
+
+    public void SetValue(int value)
+    {
+        this.value = value;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Element other = (Element)obj;
+        return value == other.value;
+    }
+
+    public override int GetHashCode()
+    {
+        return value.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return value.ToString();
+    }
+}
+
+class OneDimensionalArray
+{
+    private Element[] array;
+
+    public OneDimensionalArray(int size)
+    {
+        array = new Element[size];
+    }
+
+    public void SetElement(int index, int value)
+    {
+        if (index >= 0 && index < array.Length)
+        {
+            array[index] = new Element(value);
+        }
+    }
+
+    public int GetElement(int index)
+    {
+        if (index >= 0 && index < array.Length)
+        {
+            return array[index].GetValue();
+        }
+
+        return 0;
+    }
+
+    public void Add(OneDimensionalArray otherArray)
+    {
+        int minLength = Math.Min(array.Length, otherArray.array.Length);
+
+        for (int i = 0; i < minLength; i++)
+        {
+            array[i].SetValue(array[i].GetValue() + otherArray.array[i].GetValue());
+        }
+    }
+
+    public void Subtract(OneDimensionalArray otherArray)
+    {
+        int minLength = Math.Min(array.Length, otherArray.array.Length);
+
+        for (int i = 0; i < minLength; i++)
+        {
+            array[i].SetValue(array[i].GetValue() - otherArray.array[i].GetValue());
+        }
+    }
+
+    public void Multiply(OneDimensionalArray otherArray)
+    {
+        int minLength = Math.Min(array.Length, otherArray.array.Length);
+
+        for (int i = 0; i < minLength; i++)
+        {
+            array[i].SetValue(array[i].GetValue() * otherArray.array[i].GetValue());
+        }
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            result += array[i].ToString() + " ";
+        }
+
+        return result.Trim();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        OneDimensionalArray array1 = new OneDimensionalArray(5);
+        array1.SetElement(0, 1);
+        array1.SetElement(1, 2);
+        array1.SetElement(2, 3);
+        array1.SetElement(3, 4);
+        array1.SetElement(4, 5);
+
+        OneDimensionalArray array2 = new OneDimensionalArray(5);
+        array2.SetElement(0, 2);
+        array2.SetElement(1, 4);
+        array2.SetElement(2, 6);
+        array2.SetElement(3, 8);
+        array2.SetElement(4, 10);
+
+        Console.WriteLine("Array 1: " + array1.ToString());
+        Console.WriteLine("Array 2: " + array2.ToString());
+
+        array1.Add(array2);
+        Console.WriteLine("Array 1 + Array 2: " + array1.ToString());
+
+        array1.Subtract(array2);
+        Console.WriteLine("Array 1 - Array 2: " + array1.ToString());
+
+        array1.Multiply(array2);
+        Console.WriteLine("Array 1 * Array 2: " + array1.ToString());
+
+        Console.ReadLine();
+    }
+}
---
+++ lab010402/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+class TPair
+{
+    protected int value1;
+    protected int value2;
+
+    public TPair(int value1, int value2)
+    {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    public virtual void Increase()
+    {
+        value1++;
+        value2++;
+    }
+
+    public virtual void Decrease()
+    {
+        value1--;
+        value2--;
+    }
+
+    public override string ToString()
+    {
+        return $"({value1}, {value2})";
+    }
+}
+
+class TTime : TPair
+{
+    public TTime(int hours, int minutes) : base(hours, minutes)
+    {
+    }
+
+    public override void Increase()
+    {
+        value1 += 1;
+        if (value1 >= 24)
+        {
+            value1 = 0;
+        }
+    }
+
+    public override void Decrease()
+    {
+        value1 -= 1;
+        if (value1 < 0)
+        {
+            value1 = 23;
+        }
+    }
+
+    public int Hours
+    {
+        get { return value1; }
+    }
+
+    public int Minutes
+    {
+        get { return value2; }
+    }
+}
+
+class TMoney : TPair
+{
+    public TMoney(int hryvnia, int kopiyka) : base(hryvnia, kopiyka)
+    {
+    }
+
+    public override void Increase()
+    {
+        value1 += 1;
+        if (value1 >= 100)
+        {
+            value1 = 0;
+            value2 += 1;
+        }
+    }
+
+    public override void Decrease()
+    {
+        value1 -= 1;
+        if (value1 < 0)
+        {
+            value1 = 99;
+            value2 -= 1;
+        }
+    }
+
+    public int Hryvnia
+    {
+        get { return value1; }
+    }
+
+    public int Kopiyka
+    {
+        get { return value2; }
+    }
+
+    public override string ToString()
+    {
+        return $"{value1} грн {value2:00} коп";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 5;
+
+        Random random = new Random();
+        TTime[] times = new TTime[n];
+        TMoney[] moneys = new TMoney[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            int hours = random.Next(24);
+            int minutes = random.Next(60);
+            times[i] = new TTime(hours, minutes);
+
+            int hryvnia = random.Next(1000);
+            int kopiyka = random.Next(100);
+            moneys[i] = new TMoney(hryvnia, kopiyka);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int totalMinutes = times[i].Hours * 60 + times[i].Minutes;
+            decimal costPerMinute = (decimal)moneys[i].Hryvnia + (decimal)moneys[i].Kopiyka / 100;
+            decimal totalCost = totalMinutes * costPerMinute;
+
+            Console.WriteLine($"Робота {i + 1}:");
+            Console.WriteLine($"Тривалість: {times[i].Hours} год {times[i].Minutes} хв");
+            Console.WriteLine($"Вартість за хвилину: {moneys[i]}");
+            Console.WriteLine($"Загальна вартість: {totalCost} грн");
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}

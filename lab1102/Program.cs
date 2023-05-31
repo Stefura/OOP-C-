957bdc5f32db32a6de4ad646784143e630a602a5
aruFesT <elenacom929@gmail.com>
2023-05-31 15:46:10 +0300

    1

---
+++ lab1102/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть дату у форматі 'дд.мм.рррр':");
+        string inputDate = Console.ReadLine();
+
+        DateTime currentDate = DateTime.Now;
+        DateTime targetDate = DateTime.ParseExact(inputDate, "dd.MM.yyyy", null);
+
+        if (targetDate < currentDate)
+        {
+            Console.WriteLine("Ця дата вже минула.");
+        }
+        else
+        {
+            TimeSpan remainingDays = targetDate - currentDate;
+            int days = remainingDays.Days;
+
+            Console.WriteLine($"До вказаної дати залишилось {days} днів.");
+        }
+    }
+}

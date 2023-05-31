165555d0add23a056969beb7b8001f72c8de6af9
aruFesT <elenacom929@gmail.com>
2023-05-31 15:41:45 +0300

    1

---
+++ lab11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть початковий рядок:");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("Введіть позицію, з якої почати заміну:");
+        int position = int.Parse(Console.ReadLine());
+
+        string result = ReplaceCharacters(input, position);
+        Console.WriteLine("Результат:");
+        Console.WriteLine(result);
+    }
+
+    static string ReplaceCharacters(string input, int position)
+    {
+        char[] charArray = input.ToCharArray();
+
+        for (int i = position; i < charArray.Length; i++)
+        {
+            if (charArray[i] == '0')
+                charArray[i] = '1';
+            else if (charArray[i] == '1')
+                charArray[i] = '0';
+        }
+
+        return new string(charArray);
+    }
+}
